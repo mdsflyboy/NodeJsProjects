9e8d4c796b0a5373c53202c8705f27aa1ab38fcf
Matthew Stringer <mdsflyboy@gmail.com>
2019-07-09 22:22:16 -0700

    first commit

---
+++ practiceApp/config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+    google:{
+        clientID: '1007840807814-maisofj1prpc620g90vq9j1ic0nsfqvk.apps.googleusercontent.com',
+        clientSecret: '1Z-P_gsJrsIg6a4_RzghytCR'
+    },
+    mongodb:{
+        dbURI: 'mongodb+srv://mdsflyboy:stringer@nn-oauth-test-bxbfs.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session: {
+        cookieKey: 'matthewissupercoolandhereisthekey'
+    }
+};
